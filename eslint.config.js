42ab9c8952768884a07a279bef25b897f37c73e5
Habibur Rahman <habibur-pro>
2025-01-30 12:11:16 +0600

    config husky

---
+++ eslint.config.js
@@ -0,0 +1,19 @@
+import tsParser from "@typescript-eslint/parser";
+import tsPlugin from "@typescript-eslint/eslint-plugin";
+
+export default [
+  {
+    files: ["**/*.ts", "**/*.tsx"], // Apply to TypeScript files
+    languageOptions: {
+      parser: tsParser,
+      ecmaVersion: 2018,
+      sourceType: "module",
+    },
+    plugins: {
+      "@typescript-eslint": tsPlugin,
+    },
+    rules: {
+      // Add your custom rules here
+    },
+  },
+];
---
+++ src/app/modules/Skill/skill.service.ts
@@ -0,0 +1,9 @@
+import Skill from "./skill.model";
+
+const addSkill = async (payload: any) => {
+  const skill = await Skill.create(payload);
+  return { message: "skill added", data: skill };
+};
+
+const skillServices = { addSkill };
+export default skillServices;
