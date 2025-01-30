fa41f9036ee257e20f9db06b112e9d4d60e06492
Habibur Rahman <habibur-pro>
2025-01-30 12:24:22 +0600

    fix: fix commitlint

--- commitlint.config.js
+++ commitlint.config.js
@@ -1,1 +1,15 @@
+// [
+//  'build',
+//  'chore',
+//  'ci',
+//  'docs',
+//  'feat',
+//  'fix',
+//  'perf',
+//  'refactor',
+//  'revert',
+//  'style',
+//  'test',
+// ],
+
 export default { extends: ['@commitlint/config-conventional'] };
